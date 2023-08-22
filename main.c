2be6332232122a96fee5284a28ed151ab301216d
Hassanabdo1 <Hassanabdelftah@outlook.com>
2023-08-23 01:27:29 +0300

    by hassan Abdo

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always NULL
  */
 int main(void)
 {
     int len;
     int len2;
     unsigned int ui;
-    long *addr;
+    void *addr;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
